fd72dc497fbb2e75009b5e3d07dd0eb955fb17f9
Eric Debeau <43334934+debeaueric@users.noreply.github.com>
2021-02-02 19:22:43 +0100

    Create launch.sh

---
+++ doc_as_code/setup/launch.sh
@@ -0,0 +1,12 @@
+pip install lfdocs-conf 
+pip install rstcheck
+pip install doc8
+pip install sphinx sphinx-autobuild
+pip install sphinxcontrib-blockdiag
+pip install sphinxcontrib-needs
+pip install sphinxcontrib-plantuml
+pip install sphinxcontrib-nwdiag
+pip install sphinxcontrib-seqdiag
+pip install sphinxcontrib-swaggerdoc
+pip install sphinxcontrib-redoc
+pip install sphinx_rtd_theme
